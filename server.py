e7c4a8cbdda7d73d3dca5f32098432943abb71da
Your Name <you@example.com>
2020-01-23 12:58:26 -0800

    Flask lab

---
+++ server.py
@@ -0,0 +1,89 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+
+@app.route("/")
+def start_here():
+    """Home page."""
+
+    return """<!doctype html>
+
+            <html>Hi! This is the home page.
+
+            <a href="/hello">Link Text</a>
+
+            </html>"""
+
+
+@app.route("/hello")
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          Choose a compliment: 
+          <select name="compliment">
+            <option value="awesome">Awesome</option>
+            <option value="coolio">Coolio</option>
+            <option value="brilliant">Brilliant</option>
+            <option value="lovely">Lovely</option>
+            <option value="smashing">Smashing</option>
+            </select>
+
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route("/greet")
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    compliment = request.args.get("compliment")
+
+    # y = x
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {}! I think you're {}!
+      </body>
+    </html>
+    """.format(player, compliment)
+
+
+@
+
+
+if __name__ == "__main__":
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0", port=8000)
